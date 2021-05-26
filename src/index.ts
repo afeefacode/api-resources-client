c2646bb8d97b15d5472c6fcb784ff1dbaa778b81
Kaken Bok <bok@kakenbok.de>
2021-05-26 14:20:54 +0200

    set version: v0.0.15

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,6 @@
-import { test } from './index2'
-
-console.log(test)
-console.log('test')
+export { apiResources } from './ApiResources'
+export { ApiRequest } from './api/ApiRequest'
+export { Client } from './Client'
+export { Model } from './Model'
+export { BaseQuerySource } from './filter/BaseQuerySource'
+export { filterHistory } from './filter/FilterHistory'
