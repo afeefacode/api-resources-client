0214e3feee988bfafc16ddcb8430ebe2ca98481c
Kaken Bok <bok@kakenbok.de>
2021-06-29 14:50:06 +0200

    set version: v0.0.43

--- lib/types/type/Type.d.ts
+++ lib/types/type/Type.d.ts
@@ -3,15 +3,22 @@ export declare type TypeJSON = {
     fields: Record<string, FieldJSON>;
     update_fields: Record<string, FieldJSON>;
     create_fields: Record<string, FieldJSON>;
+    translations: Record<string, string>;
 };
 export declare class Type {
     name: string;
     private _fields;
     private _updateFields;
     private _createFields;
+    private _translations;
     constructor(name: string, json: TypeJSON);
+    getTranslations(): Record<string, string>;
+    t(key: string): string | null;
     getFields(): Record<string, Field>;
+    getField(name: string): Field | null;
     getUpdateFields(): Record<string, Field>;
+    getUpdateField(name: string): Field | null;
     getCreateFields(): Record<string, Field>;
+    getCreateField(name: string): Field | null;
 }
 //# sourceMappingURL=Type.d.ts.map
